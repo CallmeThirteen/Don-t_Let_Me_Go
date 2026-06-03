280782c0728f290cf1fe0fc57289720f871ae340
CallmeThirteen <chunbindeng9@gmail.com>
2026-06-03 23:13:04 +0900

    实现添加可交互Actor和Line Trace检测和获取相关Actor的信息

---
+++ DontLetMeGo/Source/DontLetMeGo/PickupItem.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickupItem.generated.h"
+
+UCLASS()
+class DONTLETMEGO_API APickupItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickupItem();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FString ItemName;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
